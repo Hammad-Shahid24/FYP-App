c3bf1ca803d7a57545940317a26d1d6b30d23700
Hammad-Shahid24 <apilex40@gmail.com>
2024-02-13 22:33:12 +0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fypapp/firebase_options.dart';
 import 'package:fypapp/views/home_view.dart';
 import 'package:fypapp/views/signin_view.dart';
 import 'package:fypapp/views/signup_view.dart';
@@ -7,7 +9,9 @@ import 'constants/routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
